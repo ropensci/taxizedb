e8abcf6cb452eeb1cf9fd9b81a124e845caf9ebe
Scott Chamberlain <myrmecocystus@gmail.com>
2017-02-08 17:49:49 +0100

    #7 added draft bits for a downstream fxn in inst/ignore/ file

--- R/db_download.R
+++ R/db_download.R
@@ -122,6 +122,65 @@ db_download_col <- function(verbose = TRUE){
   return( final_file )
 }
 
+#' @export
+#' @rdname db_download
+db_download_gbif <- function(verbose = TRUE){
+  # paths
+  db_url <- 'http://rs.gbif.org/datasets/backbone/backbone-current.zip'
+  db_path <- path.expand('~/.taxize_local/backbone-current.zip')
+  db_path_file <- path.expand('~/.taxize_local/backbone-current')
+  #db_sql_path <- path.expand('~/.taxize_local/gbif/gbif/gbif.sql.tar.gz')
+  taxon_file <- path.expand('~/.taxize_local/backbone-current/taxon.txt')
+  final_file <- path.expand('~/.taxize_local/gbif.sql')
+  # make home dir if not already present
+  mkhome(path.expand('~/.taxize_local/'))
+  # download data
+  mssg(verbose, 'downloading...')
+  curl::curl_download(db_url, db_path, quiet = TRUE)
+  # unzip
+  mssg(verbose, 'unzipping...')
+  unzip(db_path, exdir = path.expand('~/.taxize_local'))
+  # convert to sqlite database
+  library(DBI)
+  con <- dbConnect(RSQLite::SQLite(), path.expand('~/.taxize_local/gbif.sql'))
+  ## create table
+  dbSendQuery(con, gbif_create_table)
+  dbWriteTable(con, name = "gbif", vv)
+  ## load txt file
+  system2("sqlite3")
+  # move database
+  file.rename(file.path(db_sql_out, "gbif.sql"), final_file)
+  # cleanup
+  mssg(verbose, 'cleaning up...')
+  unlink(db_path)
+  unlink(db_path_file, recursive = TRUE)
+  return( final_file )
+}
+
+gbif_create_table <- '
+CREATE TABLE gbif (
+    id integer PRIMARY KEY,
+    taxonID integer,
+    datasetID integer,
+    parentNameUsageID integer,
+    acceptedNameUsageID integer,
+    originalNameUsageID integer,
+    scientificName varchar(300),
+    taxonRank varchar(50),
+    nameAccordingTo varchar(50),
+    namePublishedIn varchar(50),
+    taxonomicStatus varchar(200),
+    nomenclaturalStatus varchar(200),
+    kingdom varchar(200),
+    phylum varchar(200),
+    clazz varchar(200),
+    ordder varchar(200),
+    family varchar(200),
+    genus varchar(200),
+    taxonRemarks text
+);
+'
+
 ## some code tried to use to convert COL mysql db to postgresql, didn't work
 # mysqldump --compatible=postgresql --default-character-set=utf8 -r col.mysql -u root col2014ac
 # python db_converter.py col.mysql col.psql
---
+++ inst/ignore/downstream_sql.R
@@ -0,0 +1,45 @@
+#tsn <- 173420
+make_sql <- function(x) {
+  sprintf("select t.tsn, t.parent_tsn, t.complete_name as combinedName,
+                           r.rank_name, r.rank_id, a.taxon_author as author
+      from taxonomic_units t
+      left join taxon_authors_lkp a on t.taxon_author_id = a.taxon_author_id
+      inner join taxon_unit_types r on r.rank_id = t.rank_id and r.kingdom_id = t.kingdom_id
+      where (%s or %s)
+      and (t.name_usage='valid' or t.name_usage='accepted')",
+      paste(sprintf("t.parent_tsn='%s'", x), collapse = " or "),
+      paste(sprintf("t.tsn='%s'", x), collapse = " or ")
+      )
+}
+
+tsn2 <- 173420 #amphibia TSN
+out <- list()
+# pb <- txtProgressBar(min = 0, max = 10, initial = 0,
+#                      style = 3)
+for (i in 1:10) {
+  #setTxtProgressBar(pb, i)
+  # tmp <- lapply(tsn2, function(z) {
+  #   df <- sql_collect(src, make_sql(z))
+  #   filter(df, tsn != z)
+  # })
+  i <- 5
+  df <- sql_collect(src, make_sql(tsn2))
+  df$rank_name <- rcrossref:::strtrim(df$rank_name)
+
+  keep <- df %>% filter(rank_name == "Species")
+
+  df <- filter(df, !tsn %in% tsn2)
+
+  #df <- bind_rows(tmp)
+  tsn2 <- df %>% filter(rank_name != "Species") %>% .$tsn
+  #out[[i]] <- filter(df, rank_name == "Species")
+  out[[i]] <- bind_rows(df, keep)
+}
+close(pb)
+out
+all <- bind_rows(out)
+all$rank_name <- rcrossref:::strtrim(all$rank_name)
+all %>%
+  filter(rank_name == "Species") %>%
+  distinct() %>%
+  readr::write_csv(path = "amphibians.csv")
