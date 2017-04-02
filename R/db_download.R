bdf2eecb5867e5e4b5abc512931b93e15d768064
Scott Chamberlain <myrmecocystus@gmail.com>
2017-04-02 16:37:36 -0700

    strated changing gbif download method

--- R/db_download.R
+++ R/db_download.R
@@ -127,12 +127,12 @@ db_download_col <- function(verbose = TRUE){
 #' @rdname db_download
 db_download_gbif <- function(verbose = TRUE){
   # paths
-  db_url <- 'http://rs.gbif.org/datasets/backbone/backbone-current.zip'
-  db_path <- path.expand('~/.taxize_local/backbone-current.zip')
-  db_path_file <- path.expand('~/.taxize_local/backbone-current')
+  db_url <- 'https://s3-us-west-2.amazonaws.com/gbif-backbone/gbif.sqlite'
+  db_path <- path.expand('~/.taxize_local/gbif.sqlite')
+  #db_path_file <- path.expand('~/.taxize_local/backbone-current')
   #db_sql_path <- path.expand('~/.taxize_local/gbif/gbif/gbif.sql.tar.gz')
-  taxon_file <- path.expand('~/.taxize_local/backbone-current/taxon.txt')
-  final_file <- path.expand('~/.taxize_local/gbif.sql')
+  #taxon_file <- path.expand('~/.taxize_local/backbone-current/taxon.txt')
+  final_file <- path.expand('~/.taxize_local/gbif.sqlite')
   # make home dir if not already present
   mkhome(path.expand('~/.taxize_local/'))
   # download data
@@ -148,11 +148,6 @@ db_download_gbif <- function(verbose = TRUE){
   RSQLite::dbSendQuery(con, gbif_create_table)
   #RSQLite::dbWriteTable(con, name = "gbif", value = )
 
-  # sql query to load data
-  #ldq <- ".mode csv \n .import
-  #  /Users/sacmac/.taxize_local/backbone-current/taxon.txt gbif;"
-  #RSQLite::dbSendQuery(con, ldq)
-
   ## load txt file
   system2("sqlite3")
   # move database
@@ -163,33 +158,3 @@ db_download_gbif <- function(verbose = TRUE){
   unlink(db_path_file, recursive = TRUE)
   return( final_file )
 }
-
-gbif_create_table <- '
-CREATE TABLE gbif (
-    id integer PRIMARY KEY,
-    taxonID integer,
-    datasetID integer,
-    parentNameUsageID integer,
-    acceptedNameUsageID integer,
-    originalNameUsageID integer,
-    scientificName varchar(300),
-    taxonRank varchar(50),
-    nameAccordingTo varchar(50),
-    namePublishedIn varchar(50),
-    taxonomicStatus varchar(200),
-    nomenclaturalStatus varchar(200),
-    kingdom varchar(200),
-    phylum varchar(200),
-    clazz varchar(200),
-    ordder varchar(200),
-    family varchar(200),
-    genus varchar(200),
-    taxonRemarks text
-);
-'
-
-## some code tried to use to convert COL mysql db to postgresql, didn't work
-# mysqldump --compatible=postgresql --default-character-set=utf8 -r col.mysql
-#   -u root col2014ac
-# python db_converter.py col.mysql col.psql
-# psql -f col.psql
