4354a940021d6f67552d0e044ca367f00c298586
Scott Chamberlain <myrmecocystus@gmail.com>
2017-05-02 22:03:31 -0700

    started tests

---
+++ tests/testthat/test-src.R
@@ -0,0 +1,20 @@
+context("src")
+
+test_that("src structure is as expected", {
+  expect_is(src_itis, "function")
+  expect_is(src_gbif, "function")
+  expect_is(src_tpl, "function")
+  expect_is(src_col, "function")
+})
+
+
+test_that("src itis fails well", {
+  expect_error(src_itis(), "could not connect")
+  expect_error(src_col(), "Failed to connect")
+  expect_error(src_tpl(), "could not connect")
+
+  expect_error(src_gbif(), "argument \"path\" is missing")
+  expect_error(src_gbif("Asdf"), "Path does not exist")
+  f <- tempfile()
+  expect_error(src_gbif(f), "Path does not exist")
+})
