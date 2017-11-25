518b5860b205d19497057df68d6cd681b8523693
Zebulun Arendsee <arendsee@iastate.edu>
2017-11-24 18:51:06 -0600

    Make NCBI names::name_txt case insensitive
     * `classification(c('homo sapiens', 'zEa mays'))` works
    
     * case insensitivity is set at the SQL schema level, so will work for
     any command
    
     * sorted out the indexing so searching by name is almost as fast as by
     taxon ID.
    
     * added test for named search

--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -3,9 +3,14 @@ context("classification")
 library(taxize, quietly = TRUE, warn.conflicts = FALSE)
 
 test_that("taxizedb::classification == taxize::classification", {
-  taxa <- c(3702, 9606)
+  taxa_ids <- c(3702, 9606)
+  taxa_names <- c("Arabidopsis thaliana", "Homo sapiens")
   expect_equal(
-    taxize::classification(taxa, db='ncbi'),
-    taxizedb::classification(taxa, db='ncbi')
+    taxize::classification(taxa_ids, db='ncbi'),
+    taxizedb::classification(taxa_ids, db='ncbi')
+  )
+  expect_equal(
+    taxize::classification(taxa_names, db='ncbi'),
+    taxizedb::classification(taxa_names, db='ncbi')
   )
 })
