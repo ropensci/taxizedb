ab465ba4135ff298827ca06c64b1cd82be4656a7
Scott Chamberlain <myrmecocystus@gmail.com>
2018-06-06 21:17:21 -0700

    wikidata fxns should be done

--- inst/ignore/wikidata.R
+++ inst/ignore/wikidata.R
@@ -1,121 +1,36 @@
 db_download_wikidata <- function(verbose = TRUE){
-  # set paths
-  db_url <- 'ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip'
-  db_path_file <- file.path(tdb_cache$cache_path_get(), 'taxdump.zip')
-  db_path_dir <- file.path(tdb_cache$cache_path_get(), 'taxdump')
-  ncbi_names_file <- file.path(db_path_dir, 'names.dmp')
-  ncbi_nodes_file <- file.path(db_path_dir, 'nodes.dmp')
-  final_file <- file.path(tdb_cache$cache_path_get(), 'NCBI.sql')
+  # FIXME: make sure to update the URL below
+  db_url <- 'http://doi.org/10.5281/zenodo.1211767/wikidata-taxon-info20171227.tsv.gz'
+  
+  txt_file <- file.path(tdb_cache$cache_path_get(), 'wikidata-taxon-info20171227.tsv.gz')
+  final_file <- file.path(tdb_cache$cache_path_get(), 'wikidata.sqlite')
 
-  if(file.exists(final_file)){
+  if (file.exists(final_file)) {
     mssg(verbose, "Database already exists, returning old file")
     return(final_file)
   }
 
-  # make home dir if not already present
   tdb_cache$mkdir()
-  # download data
+
+  # download
   mssg(verbose, 'downloading...')
-  curl::curl_download(db_url, db_path_file, quiet = TRUE)
-  # unzip
-  mssg(verbose, 'unzipping...')
-  utils::unzip(db_path_file, files = c('names.dmp', 'nodes.dmp'), exdir = db_path_dir)
+  curl::curl_download(db_url, txt_file, quiet = TRUE)
 
-  # Taxonomy names file (names.dmp):
-  #   tax_id       -- the id of node associated with this name
-  #   name_txt     -- name itself
-  #   unique name  -- the unique variant of this name if name not unique
-  #   name class   -- (synonym, common name, ...)
-  mssg(verbose, "loading 'names.dmp'...")
-  ncbi_names <- readr::read_tsv(
-    ncbi_names_file,
-    col_names = c("tax_id", "name_txt", "unique_name", "name_class"),
-    col_type = "i_c_c_c_",
-    quote = ""
-  )
-
-  # nodes.dmp file consists of taxonomy nodes. The description for each node includes the following
-  #   tax_id                                 -- node id in GenBank taxonomy database
-  #   parent_tax_id                          -- parent node id in GenBank taxonomy database
-  #   rank                                   -- rank of this node (superkingdom, kingdom, ...)
-  #   embl_code                              -- locus-name prefix; not unique
-  #   division_id                            -- see division.dmp file
-  #   inherited_div_flag            (1 or 0) -- 1 if node inherits division from parent
-  #   genetic_code_id                        -- see gencode.dmp file
-  #   inherited_GC_flag             (1 or 0) -- 1 if node inherits genetic code from parent
-  #   mitochondrial_genetic_code_id          -- see gencode.dmp file
-  #   inherited_MGC_flag            (1 or 0) -- 1 if node inherits mitochondrial gencode from parent
-  #   GenBank_hidden_flag           (1 or 0) -- 1 if name is suppressed in GenBank entry lineage
-  #   hidden_subtree_root_flag      (1 or 0) -- 1 if this subtree has no sequence data yet
-  #   comments                               -- free-text comments and citations
-  mssg(verbose, "loading 'nodes.dmp'...")
-  ncbi_nodes <- readr::read_tsv(
-    ncbi_nodes_file,
-    col_names=c(
-      "tax_id",
-      "parent_tax_id",
-      "rank",
-      "embl_code",
-      "division_id",
-      "inherited_div_flag",
-      "genetic_code_id",
-      "inherited_GC_flag",
-      "mitochondrial_genetic_code_id",
-      "inherited_MGC_flag",
-      "GenBank_hidden_flag",
-      "hidden_subtree_root_flag",
-      "comments"
-    ),
-    col_types='i_i_c_c_i_i_i_i_i_i_i_i_c_',
-    quote=""
-  )
-
-  mssg(verbose, 'building hierarchy table...')
-  # will hold a table for every taxonomic level ascending from leaf to root the
-  # length of this list will equal the depth of the taxonomic tree (e.g. 37
-  # currently)
-  hierarchs <- list()
-  # set up the base table with columns 'tax_id', 'ancestor', and 'level', where
-  # level is 1 for immediate parent
-  hierarchs[[1]] <- ncbi_nodes[, c('tax_id', 'parent_tax_id')] %>%
-    magrittr::set_names(c('tax_id', 'ancestor'))
-  hierarchs[[1]]$level <- 1
-  # make a child to parent map
-  child2parent <- ncbi_nodes$parent_tax_id
-  names(child2parent) <- ncbi_nodes$tax_id
-  # Iteratively replace the ancestor column with the ancestor parent until all
-  # lineages converge to root. Each iteration is stored in a new table with
-  # level incremented.
-  while(TRUE){
-    top <- tail(hierarchs, 1)[[1]]
-    incomplete <- top$ancestor != 1L # 1 is the taxonomy root id
-    top <- top[incomplete, ]
-    if(nrow(top) == 0){
-      break
-    }
-    hierarchs[[length(hierarchs)+1]] <- tibble::tibble(
-      tax_id = top$tax_id,
-      ancestor = child2parent[as.character(top$ancestor)],
-      level = rep(length(hierarchs) + 1, nrow(top))
-    )
-  }
-  # Bind all levels into one table.
-  hierarchy <- do.call(rbind, hierarchs)
-  hierarchy$level <- as.integer(hierarchy$level)
-
+  # load taxa.txt
+  taxa_txt <- readr::read_tsv(txt_file, skip = 1, 
+    col_names = c('wikidata_id', 'scientific_name', 'rank_id', 'parent_id', 'external_ids'))
 
   mssg(verbose, 'building SQLite database...')
-
   db <- RSQLite::dbConnect(RSQLite::SQLite(), dbname=final_file)
 
-  # Create tables - I have to manually make the `names` table because I have to
-  # set `COLLATE NOCASE` at the time of table creation.
+  # Create table
   RSQLite::dbExecute(conn=db, "
-    CREATE TABLE names (
-      tax_id INTEGER,
-      name_txt TEXT COLLATE NOCASE,
-      unique_name TEXT,
-      name_class TEXT
+    CREATE TABLE wikidata (
+      wikidata_id TEXT,
+      scientific_name TEXT,
+      rank_id TEXT,
+      parent_id TEXT,
+      external_ids TEXT
     )
     "
   )
@@ -123,49 +38,27 @@ db_download_wikidata <- function(verbose = TRUE){
   # Load tables
   RSQLite::dbWriteTable(
     conn   = db,
-    name   = 'names',
-    value  = as.data.frame(ncbi_names),
-    append = TRUE # since I explicitly created the table above
-  )
-  RSQLite::dbWriteTable(
-    conn  = db,
-    name  = 'nodes',
-    value = as.data.frame(ncbi_nodes),
-  )
-  RSQLite::dbWriteTable(
-    conn  = db,
-    name  = 'hierarchy',
-    value = as.data.frame(hierarchy),
+    name   = 'wikidata',
+    value  = as.data.frame(taxa_txt),
+    append = TRUE
   )
 
-  # Create indices on tax_id columns
+  # Create indices on taxonID columns
   RSQLite::dbExecute(db,
-    'CREATE INDEX tax_id_index_names ON names (tax_id)'
+    'CREATE INDEX wikidata_id_index ON wikidata (wikidata_id)'
   )
   RSQLite::dbExecute(db,
-    'CREATE INDEX name_txt_index_names ON names (name_txt COLLATE NOCASE)'
-  )
-  RSQLite::dbExecute(db,
-    'CREATE INDEX tax_id_index_nodes ON nodes (tax_id)'
-  )
-  RSQLite::dbExecute(db,
-    'CREATE INDEX tax_id_index_hierarchy ON hierarchy (tax_id)'
-  )
-  RSQLite::dbExecute(db,
-    'CREATE INDEX tax_id_ancestor_hierarchy ON hierarchy (ancestor)'
+    'CREATE INDEX scientific_name_index ON wikidata (scientific_name)'
   )
 
   RSQLite::dbDisconnect(db)
 
   # cleanup
   mssg(verbose, 'cleaning up...')
-  unlink(db_path_file)
-  unlink(db_path_dir, recursive = TRUE)
-
+  unlink(txt_file)
+  mssg(verbose, 'all done...')
   return(final_file)
 }
-
-
 
 #' @export
 #' @rdname db_load
