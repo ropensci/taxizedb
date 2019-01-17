7ee9741a2ed5bcd28cd540a8d7b2638fe24737f6
Zebulun Arendsee <arendsee@iastate.edu>
2019-01-17 17:09:12 -0600

    Added test for #44
    This ONLY tests that an error rises for ambiguous names. It does NOT
    test the error message itself. Such a test feels a little too fine-grain
    for my tastes.

--- tests/testthat/test-downstream.R
+++ tests/testthat/test-downstream.R
@@ -6,3 +6,7 @@ test_that("taxizedb::downstream matches taxize::downstream", {
     taxize::downstream('Arabidopsis', db='ncbi', downto='species')
   )
 })
+
+test_that("downstream dies on ambiguities", {
+  expect_error(taxizedb::downstream("Bacteria"))
+})
