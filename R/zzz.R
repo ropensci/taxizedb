f1570850db22fc8dd2dd5340363ee9d854822735
Scott Chamberlain <myrmecocystus@gmail.com>
2020-08-13 11:20:13 -0700

    remove fxns not used from zzz.r

--- R/zzz.R
+++ R/zzz.R
@@ -1,33 +1,4 @@
 mssg <- function(v, ...) if (v) message(...)
-
-txdbc <- function(x) Filter(Negate(is.null), x)
-
-cl <- function(x, y){
-  if (is.null(y)) {
-    ""
-  } else {
-    paste0(x, y)
-  }
-}
-
-mkhome <- function(x) {
-  dir.create(x, showWarnings = FALSE, recursive = FALSE)
-}
-
-# db_installed(x = 'psql')
-db_installed <- function(x) {
-  tmp <- Sys.which(x)
-  if (any(tmp == "")) {
-    nf <- paste0(names(tmp)[tmp == ""], collapse = ", ")
-    stop(
-      sprintf(
-  "\n%s not found on your computer\nInstall the missing tool(s) and try again",
-  nf))
-  }
-}
-
-sm <- function(x) suppressMessages(x)
-
 assert <- function(x, y) {
   if (!is.null(x)) {
     if (!inherits(x, y)) {
