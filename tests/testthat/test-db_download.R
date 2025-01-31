a179b7954d1fcad42974979355be45d1e924cfc6
Tamas Stirling <stirling.tamas@gmail.com>
2025-01-31 16:44:59 +0100

    Test that existing TPL database can still be imported

--- tests/testthat/test-db_download.R
+++ tests/testthat/test-db_download.R
@@ -11,3 +11,20 @@ test_that("db_download - are functions", {
   expect_named(formals(db_download_itis), c("verbose", "overwrite"))
   expect_named(formals(db_download_tpl), c("verbose", "overwrite"))
 })
+
+test_that("db_download_tpl()", {
+  file <- file.path(tdb_cache$cache_path_get(), 'plantlist.sqlite')
+  if (file.exists(file)) {
+    # existing file can be imported
+    suppressMessages(expect_no_error(db_download_tpl()))
+    expect_equal(
+      capture_messages(db_download_tpl()),
+      "Database already exists, returning old file\n"
+    )
+    # existing file cannot be updated
+    expect_error(db_download_tpl(overwrite = TRUE))
+  } else {
+    # new file cannot be downloaded
+    expect_error(db_download_tpl())
+  }
+})
